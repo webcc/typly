4748beebbbe88791720e964c591d59b6e20ad475
cleancoderocker <scalarocker@googlemail.com>
2015-09-24 15:38:21 +0200

    Replaced function definitions with arrow functions

--- test/typly-test.js
+++ test/typly-test.js
@@ -2,69 +2,69 @@
 let assert = require('assert');
 let typly = require("../lib/typly");
 
-describe("typly", function ()
+describe("typly", () =>
 {
-  describe("#isInstanceOf", function ()
+  describe("#isInstanceOf", () =>
   {
-    it("should pass for objects", function ()
+    it("should pass for objects", () =>
     {
       assert.ok(typly.isInstanceOf({}, Object));
       assert.ok(typly.isInstanceOf(new Object(), Object));
     });
-    it("should return false for null", function ()
+    it("should return false for null", () =>
     {
       assert.ok(!typly.isInstanceOf(null, Object));
     });
   });
-  describe("#assertInstanceOf", function ()
+  describe("#assertInstanceOf", () =>
   {
-    it("should throw a TypeError for numbers", function ()
+    it("should throw a TypeError for numbers", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertInstanceOf(5, Object);
       }, TypeError);
     });
-    it("should throw a TypeError for null", function ()
+    it("should throw a TypeError for null", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertInstanceOf(null, Object);
       }, TypeError);
     });
   });
-  describe("#isNumber", function ()
+  describe("#isNumber", () =>
   {
-    it("should pass for numbers", function ()
+    it("should pass for numbers", () =>
     {
       assert.ok(typly.isNumber(5));
     });
-    it("should return false for null", function ()
+    it("should return false for null", () =>
     {
       assert.ok(!typly.isNumber(null));
     });
   });
-  describe("#assertNumber", function ()
+  describe("#assertNumber", () =>
   {
-    it("should throw a TypeError for objects", function ()
+    it("should throw a TypeError for objects", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertNumber({});
       }, TypeError);
     });
-    it("should throw a TypeError with correct message", function ()
+    it("should throw a TypeError with correct message", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertNumber({});
       }, function(error) {
         return error.message === "number expected, but got Object";
       });
     });
-    it("should throw a RangeError for an invalid range", function ()
+    it("should throw a RangeError for an invalid range", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertNumber(9, {
           min: 0,
@@ -76,161 +76,161 @@ describe("typly", function ()
         max: 5
       }));
     });
-    it("should throw a TypeError for null", function ()
+    it("should throw a TypeError for null", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertNumber(null);
       }, TypeError);
     });
   });
-  describe("#isString", function ()
+  describe("#isString", () =>
   {
-    it("should pass for strings", function ()
+    it("should pass for strings", () =>
     {
       assert.ok(typly.isString("Example"));
     });
-    it("should return false for null", function ()
+    it("should return false for null", () =>
     {
       assert.ok(!typly.isString(null));
     });
   });
-  describe("#assertString", function ()
+  describe("#assertString", () =>
   {
-    it("should throw a TypeError for objects", function ()
+    it("should throw a TypeError for objects", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertString({});
       }, TypeError);
     });
-    it("should throw a TypeError for null", function ()
+    it("should throw a TypeError for null", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertString(null);
       }, TypeError);
     });
   });
-  describe("#isUri", function ()
+  describe("#isUri", () =>
   {
-    it("should pass for uris", function ()
+    it("should pass for uris", () =>
     {
       assert.ok(typly.isUri("http://www.example.com"));
     });
-    it("should return false for null", function ()
+    it("should return false for null", () =>
     {
       assert.ok(!typly.isUri(null));
     });
   });
-  describe("#assertUri", function ()
+  describe("#assertUri", () =>
   {
-    it("should throw a TypeError for invalid uris", function ()
+    it("should throw a TypeError for invalid uris", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         assert.ok(typly.assertUri("Example"));
       }, TypeError);
     });
-    it("should throw a TypeError for null", function ()
+    it("should throw a TypeError for null", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertUri(null);
       }, TypeError);
     });
   });
-  describe("#isArray", function ()
+  describe("#isArray", () =>
   {
-    it("should pass for arrays", function ()
+    it("should pass for arrays", () =>
     {
       assert.ok(typly.isArray([]));
       assert.ok(typly.isArray(new Array()));
     });
-    it("should return false for null", function ()
+    it("should return false for null", () =>
     {
       assert.ok(!typly.isArray(null));
     });
   });
-  describe("#assertArray", function ()
+  describe("#assertArray", () =>
   {
-    it("should throw a TypeError for objects", function ()
+    it("should throw a TypeError for objects", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertArray({});
       }, TypeError);
     });
-    it("should throw a TypeError for null", function ()
+    it("should throw a TypeError for null", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertArray(null);
       }, TypeError);
     });
   });
-  describe("#isBoolean", function ()
+  describe("#isBoolean", () =>
   {
-    it("should pass for booleans", function ()
+    it("should pass for booleans", () =>
     {
       assert.ok(typly.isBoolean(true));
       assert.ok(typly.isBoolean(false));
     });
-    it("should return false for null", function ()
+    it("should return false for null", () =>
     {
       assert.ok(!typly.isBoolean(null));
     });
   });
-  describe("#assertBoolean", function ()
+  describe("#assertBoolean", () =>
   {
-    it("should throw a TypeError for objects", function ()
+    it("should throw a TypeError for objects", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertBoolean({});
       }, TypeError);
     });
-    it("should throw a TypeError for null", function ()
+    it("should throw a TypeError for null", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertBoolean(null);
       }, TypeError);
     });
   });
-  describe("#isDate", function ()
+  describe("#isDate", () =>
   {
-    it("should pass for dates", function ()
+    it("should pass for dates", () =>
     {
       assert.ok(typly.isDate(new Date()));
     });
-    it("should return false for null", function ()
+    it("should return false for null", () =>
     {
       assert.ok(!typly.isDate(null));
     });
   });
-  describe("#assertDate", function ()
+  describe("#assertDate", () =>
   {
-    it("should throw a TypeError for objects", function ()
+    it("should throw a TypeError for objects", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertDate({});
       }, TypeError);
     });
-    it("should throw a TypeError for null", function ()
+    it("should throw a TypeError for null", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertDate(null);
       }, TypeError);
     });
   });
-  describe("#config", function ()
+  describe("#config", () =>
   {
-    it("should allow to set ignoring null values", function ()
+    it("should allow to set ignoring null values", () =>
     {
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertNumber(null);
       }, TypeError);
@@ -241,7 +241,7 @@ describe("typly", function ()
       typly.config({
         ignoreNullValues: false
       });
-      assert.throws(function ()
+      assert.throws(() =>
       {
         typly.assertNumber(null);
       }, TypeError);
