97940165463ec2b7066a6e3a27cb74ee2849c9da
darthcav <darth.cav@gmail.com>
2015-10-24 16:21:50 +0200

    example update

--- examples/setter-validation.js
+++ examples/setter-validation.js
@@ -1,24 +1,37 @@
 "use strict";
+
 let typly = require("typly");
+
 class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-  get firstName() {
-    return this._firstName;
-  }
-  set firstName(firstName) {
-    typly.assertString(firstName);
-    this._firstName = firstName;
-  }
-  get lastName() {
-    return this._lastName;
-  }
-  set lastName(lastName) {
-    typly.assertString(lastName);
-    this._lastName = lastName;
-  }
+    constructor(firstName, lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get firstName()
+    {
+        return this._firstName;
+    }
+
+    set firstName(firstName)
+    {
+        typly.assertString(firstName);
+        this._firstName = firstName;
+    }
+
+    get lastName()
+    {
+        return this._lastName;
+    }
+
+    set lastName(lastName)
+    {
+        typly.assertString(lastName);
+        this._lastName = lastName;
+    }
 }
-let max = new Person('Max', 'Mustermann');
-let somebodyElse = new Person(4711, 'Mustermann');  // Output: TypeError: string expected, but got number
+
+let max = new Person("Max", "Mustermann");
+let somebodyElse = new Person(4711, "Mustermann");
+// Output: TypeError: [tiply] Expected type string, but got type number
