3b1a7f1525c6882bb57bb7d5424ff7d7eb3f794d
Evangelos Vlachogiannis <evlach@fit.fraunhofer.de>
2015-11-20 13:10:26 +0100

    fix(isInstanceOf): fallback for known instanceof keyword issue in nodejs/npm when having multiple instances of same module

--- test/InstanceTest.js
+++ test/InstanceTest.js
@@ -4,13 +4,48 @@ describe("typly#ObjectTest", () =>
 {
     let assert = require("assert");
     let typly = require("..");
-
+    class FooClass {
+        constructor(){
+            this.test = "tttt";
+        }
+        run(param){
+            return this.test + "Hallo";
+        }
+    }
+    class DeepFooClass extends FooClass{
+        constructor(){
+            super();
+            this.test = "tttt";
+            this.myFoo = new FooClass();
+        }
+    }
+    let FooFunc = function(){
+        this.test="test";
+        this.run = function(param){
+            return this.test + "Hallo";
+        }
+    };
     describe("#isInstanceOf", () =>
     {
         it("should return true for objects", () =>
         {
             assert(typly.isInstanceOf({}, Object));
             assert(typly.isInstanceOf(new Object(), Object));
+        });
+        it("should find instance of FooFunc", function ()
+        {
+            let foo = new FooFunc();
+            assert.equal(typly.isInstanceOf(foo, FooFunc), true);
+        });
+        it("should find instance of FooClass", function ()
+        {
+            let foo = new FooClass();
+            assert.equal(typly.isInstanceOf(foo, FooClass), true);
+        });
+        it("should find instance of DeepFooClass", function ()
+        {
+            let foo = new DeepFooClass();
+            assert.equal(typly.isInstanceOf(foo, DeepFooClass), true);
         });
     });
     describe("#assertInstanceOf", () =>
